6748bcf63f92b36ef3f2a5977f53a3063021eb68
kh <527756694@qq.com>
2023-05-11 04:48:54 +0800

    add mongodb enable filter

--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -11,6 +11,9 @@ import (
 var ctx = context.Background()
 
 func NewMongo(config Config) *mongo.Database {
+	if !config.Enable {
+		return nil
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(config.Url))
